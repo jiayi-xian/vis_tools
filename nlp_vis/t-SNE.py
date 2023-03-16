415fee7f34952855087e9ae06be319637ec13735
Jiayi Xian <jiayixian@jiayis-air.mynetworksettings.com>
2023-03-16 08:57:20 -0400

    init nlp_vis

---
+++ nlp_vis/t-SNE.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+import matplotlib.pyplot as plt
+from gensim.models import word2vec
+
+from sklearn.manifold import TSNE
+
+
+
+STOP_WORDS = nltk.corpus.stopwords.words()
+
+#%%
+print("Test")
+
+# %%
